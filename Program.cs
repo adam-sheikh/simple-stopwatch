f161aa85ffdec833c74d43707a8818858ce5b158
Adam Sheikh <47800876+adam-sheikh@users.noreply.github.com>
2019-03-23 18:50:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace Classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            string command;
+
+            Console.WriteLine("type 'start', 'stop' or 'quit'");
+            command = Console.ReadLine().ToLower();
+
+            while(command != "quit")
+            {
+                command = command.ToLower();
+                if (command == "start")
+                {
+                    stopWatch.Start();
+                    command = Console.ReadLine().ToLower();
+                }
+                else if (command == "stop")
+                {
+                    stopWatch.Stop();
+                    command = Console.ReadLine().ToLower();
+                }
+                else if (command == "pause")
+                {
+                    stopWatch.Pause();
+                    command = Console.ReadLine().ToLower();
+                }
+                else if (command == "resume")
+                {
+                    stopWatch.Resume();
+                    command = Console.ReadLine().ToLower();
+                }
+                else
+                {
+                    Console.WriteLine("invalid command. Please type 'start', 'stop' or 'quit'");
+                    command = Console.ReadLine().ToLower();
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class StopWatch
+    {
+        private bool IsRunning { set; get; }
+        private bool IsPaused { get; set; }
+
+        private TimeSpan _duration, _pauseDuration;
+        private DateTime _startTime, _pauseTime, _resumeTime;
+
+        public void Start()
+        {
+            if ((IsRunning == false) && (IsPaused == false))
+            {
+                IsRunning = true;
+                _startTime = DateTime.Now;
+                Console.WriteLine("Stopwatch start time: {0}\n", _startTime);
+            }
+            else
+            {
+                Console.WriteLine("Stopwatch is already running");
+            }
+        }
+
+        public void Pause()
+        {
+            if((IsPaused == false) && (IsRunning == true))
+            {
+                IsPaused = true;
+                IsRunning = false;
+                _pauseTime = DateTime.Now;
+                Console.WriteLine("Stopwatch paused");
+            }
+            else
+            {
+                Console.WriteLine("Stop watch is not running");
+            }
+        }
+
+        public void Resume()
+        {
+            if((IsRunning == false))
+            {
+                if (IsPaused == true)
+                {
+                    _resumeTime = DateTime.Now;
+                    _pauseDuration = _pauseDuration.Add(_resumeTime - _pauseTime);
+                    IsPaused = false;
+                    IsRunning = true;
+                    Console.WriteLine("Stopwatch resumed");
+                }
+                else
+                {
+                    Console.WriteLine("Stopwatch is not currently paused");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Stopwatch is running");
+            }
+        }
+
+        public void Stop()
+        {
+            if (IsRunning == true)
+            {
+                _duration = DateTime.Now - _startTime;
+                _duration = _duration.Subtract(_pauseDuration);
+                IsRunning = false;
+                IsPaused = false;
+                DisplayDuration();
+            }
+            else
+            {
+                Console.WriteLine("Stopwatch is not currently running");
+            }
+        }
+
+        public void DisplayDuration()
+        {
+            Console.WriteLine("Total time: {0}", _duration);
+        }
+
+    }
+}
